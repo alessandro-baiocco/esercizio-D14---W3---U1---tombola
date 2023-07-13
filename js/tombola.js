fdf6539e66ce25919d4dab88fc0c91d777e840c6
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-07-13 17:57:23 +0200

    fixed some issues

--- js/tombola.js
+++ js/tombola.js
@@ -3,6 +3,10 @@ const tabellina = document.getElementsByClassName("tabellaGiocatore")[0];
 const tabellina2 = document.getElementsByClassName("tabellaGiocatore2")[0];
 const tabellina3 = document.getElementsByClassName("tabellaGiocatore3")[0];
 const mainTable = document.querySelector("nav #generateMain");
+const genArray = [];
+const scheda1 = [];
+const scheda2 = [];
+const scheda3 = [];
 
 const generateCell = function () {
   for (i = 1; i < 76; i++) {
@@ -13,67 +17,86 @@ const generateCell = function () {
     tabellone.appendChild(cell);
   }
 };
-function getRandomInt(Num) {
-  return Math.floor(Math.random() * 75);
-}
 generateCell();
-
-const getRandomCell = function (numEvent) {
-  const rNum = Math.floor(Math.random() * 75 + 1);
-  const cellUscita = document.getElementsByClassName(`${rNum}`)[0];
-  // const cellUscita2 = document.getElementsByClassName
-  if (cellUscita === document.getElementsByClassName("uscito")[rNum]) {
-    getRandomCell("");
-    console.log("gia uscito");
-  } else {
+const generateInt = function (num) {
+  const aNumGen = Math.floor(Math.random() * 75 + 1);
+  const aNumGen2 = parseInt(aNumGen);
+  if (!genArray.includes(aNumGen2)) {
+    console.log(genArray);
+    genArray.push(aNumGen2);
+    console.log(aNumGen2);
+    const cellUscita = document.getElementsByClassName(`${aNumGen2}`)[0];
+    const cellUscita2 = document.getElementsByClassName(`${aNumGen2}`)[1];
+    const cellUscita3 = document.getElementsByClassName(`${aNumGen2}`)[2];
+    const cellUscita4 = document.getElementsByClassName(`${aNumGen2}`)[3];
     cellUscita.classList.add("uscito");
-    // cellUscita2.classList.add("uscito");
+    cellUscita2.classList.add("uscito");
+    cellUscita3.classList.add("uscito");
+    cellUscita4.classList.add("uscito");
+  } else {
+    console.log("duplicato");
+    generateInt(1);
   }
-
-  console.log(rNum);
-  // console.log(cellUscita);
 };
 
 const newTab = function (createEvent) {
-  const scheda = document.querySelectorAll(".tabellaGiocatore div");
-  if (scheda.length < 24) {
-    for (i = 1; i < 25; i++) {
-      const arrayForNumber = [];
-      cell = document.createElement("div");
-      const rNum = Math.floor(Math.random() * 75 + 1);
-      arrayForNumber.push(rNum);
-      for (i = 0; i < 23; i++) {
-        cell.classList.add(`${arrayForNumber[i]}`);
-        cell.classList.add("small");
-        cell.innerText = rNum;
-        tabellina2.appendChild(cell);
-      }
+  const generateInt2 = function () {
+    const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
+    if (!scheda1.includes(aNumGen)) {
+      scheda1.push(aNumGen);
+      console.log(aNumGen);
+      cell.classList.add(`${aNumGen}`);
+      cell.classList.add("small");
+      cell.innerText = aNumGen;
+      tabellina.appendChild(cell);
+    } else {
+      console.log("duplicato");
+      generateInt2();
     }
+  };
+
+  for (i = 1; i < 25; i++) {
+    cell = document.createElement("div");
+    generateInt2();
   }
 };
 const newTab2 = function (createEvent) {
-  const scheda = document.querySelectorAll(".tabellaGiocatore div");
-  if (scheda.length < 24) {
-    for (i = 1; i < 25; i++) {
-      cell = document.createElement("div");
-      const rNum = Math.floor(Math.random() * 75 + 1);
-      cell.classList.add(`${rNum}`);
+  const generateInt2 = function () {
+    const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
+    if (!scheda2.includes(aNumGen)) {
+      scheda2.push(aNumGen);
+      console.log(aNumGen);
+      cell.classList.add(`${aNumGen}`);
       cell.classList.add("small");
-      cell.innerText = rNum;
-      tabellina3.appendChild(cell);
+      cell.innerText = aNumGen;
+      tabellina2.appendChild(cell);
+    } else {
+      console.log("duplicato");
+      generateInt2();
     }
+  };
+  for (i = 1; i < 25; i++) {
+    cell = document.createElement("div");
+    generateInt2();
   }
 };
 const newTab3 = function (createEvent) {
-  const scheda = document.querySelectorAll(".tabellaGiocatore div");
-  if (scheda.length < 24) {
-    for (i = 1; i < 25; i++) {
-      cell = document.createElement("div");
-      const rNum = Math.floor(Math.random() * 75 + 1);
-      cell.classList.add(`${rNum}`);
+  const generateInt2 = function () {
+    const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
+    if (!scheda3.includes(aNumGen)) {
+      scheda3.push(aNumGen);
+      console.log(aNumGen);
+      cell.classList.add(`${aNumGen}`);
       cell.classList.add("small");
-      cell.innerText = rNum;
-      tabellina.appendChild(cell);
+      cell.innerText = aNumGen;
+      tabellina3.appendChild(cell);
+    } else {
+      console.log("duplicato");
+      generateInt2();
     }
+  };
+  for (i = 1; i < 25; i++) {
+    cell = document.createElement("div");
+    generateInt2();
   }
 };
