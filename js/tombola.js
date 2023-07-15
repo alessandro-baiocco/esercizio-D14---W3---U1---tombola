562b0ea739f4a26ce45ed3558fd5994f99aa52a8
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-07-15 16:35:10 +0200

    better tombola

--- js/tombola.js
+++ js/tombola.js
@@ -1,8 +1,9 @@
 const tabellone = document.querySelector("main div");
-const tabellina = document.getElementsByClassName("tabellaGiocatore")[0];
-const tabellina2 = document.getElementsByClassName("tabellaGiocatore2")[0];
-const tabellina3 = document.getElementsByClassName("tabellaGiocatore3")[0];
 const mainTable = document.querySelector("nav #generateMain");
+const numberOut = document.getElementsByClassName("numberOut")[0];
+const btnNumb = document.getElementsByClassName("newTab")[0];
+const divFooter = document.querySelector("footer div");
+
 const genArray = [];
 const scheda1 = [];
 const scheda2 = [];
@@ -19,38 +20,38 @@ const generateCell = function () {
 };
 generateCell();
 const generateInt = function (num) {
-  const aNumGen = Math.floor(Math.random() * 75 + 1);
-  const aNumGen2 = parseInt(aNumGen);
-  if (!genArray.includes(aNumGen2)) {
+  const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
+  numberOut.innerText = `${aNumGen}`;
+  if (!genArray.includes(aNumGen)) {
     console.log(genArray);
-    genArray.push(aNumGen2);
-    console.log(aNumGen2);
-    const cellUscita = document.getElementsByClassName(`${aNumGen2}`)[0];
-    const cellUscita2 = document.getElementsByClassName(`${aNumGen2}`)[1];
-    const cellUscita3 = document.getElementsByClassName(`${aNumGen2}`)[2];
-    const cellUscita4 = document.getElementsByClassName(`${aNumGen2}`)[3];
-    cellUscita.classList.add("uscito");
-    cellUscita2.classList.add("uscito");
-    cellUscita3.classList.add("uscito");
-    cellUscita4.classList.add("uscito");
+    genArray.push(aNumGen);
+    console.log(aNumGen);
+    const cellUscita = document.getElementsByClassName(`${aNumGen}`);
+    for (let i = 0; i < cellUscita.length; i++) {
+      cellUscita[i].classList.add("uscito");
+    }
+    const numeroFooter = document.createElement("div");
+    numeroFooter.innerText = `${aNumGen}`;
+    numeroFooter.classList.add("small");
+    divFooter.appendChild(numeroFooter);
   } else {
     console.log("duplicato");
     generateInt(1);
   }
 };
+let tabI = 0;
+let tabellina = document.getElementsByClassName("tabellaGiocatore");
 
 const newTab = function (createEvent) {
   const generateInt2 = function () {
     const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
     if (!scheda1.includes(aNumGen)) {
       scheda1.push(aNumGen);
-      console.log(aNumGen);
       cell.classList.add(`${aNumGen}`);
       cell.classList.add("small");
       cell.innerText = aNumGen;
-      tabellina.appendChild(cell);
+      tabellina[tabI].appendChild(cell);
     } else {
-      console.log("duplicato");
       generateInt2();
     }
   };
@@ -59,44 +60,10 @@ const newTab = function (createEvent) {
     cell = document.createElement("div");
     generateInt2();
   }
-};
-const newTab2 = function (createEvent) {
-  const generateInt2 = function () {
-    const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
-    if (!scheda2.includes(aNumGen)) {
-      scheda2.push(aNumGen);
-      console.log(aNumGen);
-      cell.classList.add(`${aNumGen}`);
-      cell.classList.add("small");
-      cell.innerText = aNumGen;
-      tabellina2.appendChild(cell);
-    } else {
-      console.log("duplicato");
-      generateInt2();
-    }
-  };
-  for (i = 1; i < 25; i++) {
-    cell = document.createElement("div");
-    generateInt2();
-  }
-};
-const newTab3 = function (createEvent) {
-  const generateInt2 = function () {
-    const aNumGen = parseInt(Math.floor(Math.random() * 75 + 1));
-    if (!scheda3.includes(aNumGen)) {
-      scheda3.push(aNumGen);
-      console.log(aNumGen);
-      cell.classList.add(`${aNumGen}`);
-      cell.classList.add("small");
-      cell.innerText = aNumGen;
-      tabellina3.appendChild(cell);
-    } else {
-      console.log("duplicato");
-      generateInt2();
-    }
-  };
-  for (i = 1; i < 25; i++) {
-    cell = document.createElement("div");
-    generateInt2();
+
+  tabI += 1;
+  console.log(tabI);
+  if (tabI === 3) {
+    btnNumb.remove();
   }
 };
